d5f3bb0a69c50330f05c9b37746416d2a280f81f
z2x <zhufind@yeah.net>
2018-03-06 21:59:36 +0800

    Complete the basic framework

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: 'test',
+      todoList: [{
+        id: 1,
+        title: 'hello world'
+      }]
+    };
+  }
+
   render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return <li>{item.title}</li>;
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
       </div>
     );
   }
