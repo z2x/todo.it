8916f678fb1da0ac96c4079bb68917187f13cf40
z2x <zhufind@yeah.net>
2018-03-06 22:36:38 +0800

    add reset.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import 'normalize.css';
+import './reset.css';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
