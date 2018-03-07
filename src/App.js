1b517989358f16f1d4404946e8a0f96b7d4fedef
z2x <zhufind@yeah.net>
2018-03-07 12:00:02 +0800

    Achieve todo delete

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ class App extends Component {
   }
 
   render() {
-    let todos = this.state.todoList.map((item, index) => {
+    let todos = this.state.todoList.filter((item) => !item.deleted).map((item, index) => {
       return (
-        <li key={index}>
+        <li key={index} >
           <TodoItem todo={item} onToggle={this.toggle.bind(this)} onDelete={this.delete.bind(this)}/>
         </li>
       );
@@ -69,7 +69,7 @@ class App extends Component {
   }
 
   delete(event, todo) {
-    todo.delete = true;
+    todo.deleted = true;
     this.setState(this.state);
   }
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,18 +4,15 @@ class TodoItem extends Component {
   render() {
     return (
       <div>
-        <input type="checkbox" checked={this.props.todo.status === 'completed'} onChange={this.toggle.bind(this)}/>
-        {this.props.todo.title}
-
-        <button onClick={this.delete.bind(this)}>delete</button>
+        <input type="checkbox" checked={this.props.todo.status === 'completed'}
+      onChange={this.toggle.bind(this)}/> {this.props.todo.title}
+        <button onClick={this.delete.bind(this)}>删除</button>
       </div>
     );
   }
-
   toggle(e) {
     this.props.onToggle(e, this.props.todo);
   }
-
   delete(e) {
     this.props.onDelete(e, this.props.todo);
   }
