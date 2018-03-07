1fed6bbd37251d80c1aa0c41db8f319dfaf4f7c9
z2x <zhufind@yeah.net>
2018-03-07 11:27:21 +0800

    add list function

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     let todos = this.state.todoList.map((item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       );
@@ -34,7 +34,7 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
@@ -42,6 +42,25 @@ class App extends Component {
       </div>
     );
   }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    });
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    });
+  }
 }
 
 export default App;
+
+let id = 0;
+
+function idMaker() {
+  id += 1;
+  return id;
+}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class TodoInput extends Component {
   render() {
     return <input type="text" defaultValue={this.props.content}
-      onKeyPress={this.submit} />;
+      onKeyPress={this.submit.bind(this)} />;
   }
 
   submit(e) {
     if (e.key === 'Enter') {
-      console.log('press Enter');
+      this.props.onSubmit(e);
     }
   }
 }
