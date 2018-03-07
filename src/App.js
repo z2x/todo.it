a836c7ec3d0df2f5142d200c88f84243ecc403ed
z2x <zhufind@yeah.net>
2018-03-07 11:42:40 +0800

    Marked as completed / not completed

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     let todos = this.state.todoList.map((item, index) => {
       return (
         <li key={index}>
-          <TodoItem todo={item} />
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
         </li>
       );
     });
@@ -43,6 +43,11 @@ class App extends Component {
         </ol>
       </div>
     );
+  }
+
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed';
+    this.setState(this.state);
   }
 
   changeTitle(event) {
