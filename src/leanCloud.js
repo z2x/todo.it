089149fb7c5c4db275e3ee554504805191bdb719
z2x <zhufind@yeah.net>
2018-03-10 19:41:34 +0800

    add leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage';
+
+let APP_ID = 'WiamNEuR1RvvIikVyFhpAbUB-gzGzoHsz';
+let APP_KEY = 'ewLqQgvGfn4Hcjs2xxfGxE4L';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV;
