e07c94dbcb1ca5ad79da30319da5792adbf1504e
z2x <zhufind@yeah.net>
2018-03-10 19:39:33 +0800

    delete Leancloud test code

--- src/App.js
+++ src/App.js
@@ -7,19 +7,11 @@ import TodoItem from './TodoItem';
 import * as localStore from './localStore';
 import AV from 'leancloud-storage';
 
-var APP_ID = 'WiamNEuR1RvvIikVyFhpAbUB-gzGzoHsz';
-var APP_KEY = 'ewLqQgvGfn4Hcjs2xxfGxE4L';
+let APP_ID = 'WiamNEuR1RvvIikVyFhpAbUB-gzGzoHsz';
+let APP_KEY = 'ewLqQgvGfn4Hcjs2xxfGxE4L';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
-})
-
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
 });
 
 class App extends Component {
