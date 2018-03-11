fa288f5e7066883b8cb731c032f4cd038b54487a
z2x <zhufind@yeah.net>
2018-03-11 15:44:22 +0800

    Click Register to register with LeanCloud API

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -8,3 +8,25 @@ AV.init({
 });
 
 export default AV;
+
+export function signUp(username, password, successFn, errorFn) {
+  let user = new AV.User();
+  user.setUsername(username);
+  user.setPassword(password);
+  user.signUp().then(function(loginedUser) {
+    let user = getUserFromAVUser(loginedUser);
+    successFn.call(null, user);
+  }, function(error) {
+    errorFn.call(null, error);
+  });
+
+  return undefined;
+
+}
+
+function getUserFromAVUser(AVUser) {
+  return {
+    id: AVUser.id,
+    ...AVUser.attributes
+  };
+}
