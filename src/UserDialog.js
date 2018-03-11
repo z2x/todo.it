9babcbff96a27878c9692d3cf49857600be02436
z2x <zhufind@yeah.net>
2018-03-11 15:16:03 +0800

    Switch between registration and login forms

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -13,41 +13,50 @@ class UserDialog extends Component {
       selected: e.target.value
     });
   }
+
   render() {
+    let signUpForm = (
+    <form action="" className="signUp">
+        <div className="row">
+          <label htmlFor="">user name</label>
+          <input type="text"/>
+        </div>
+        <div className="row">
+          <label htmlFor="">password</label>
+          <input type="password"/>
+        </div>
+        <div className="row actions">
+          <button type="submit">sign up</button>
+        </div>
+      </form>
+    );
+
+    let signInForm = (
+    <form action="" className="signUp">
+        <div className="row">
+          <label htmlFor="">user name</label>
+          <input type="text"/>
+        </div>
+        <div className="row">
+          <label htmlFor="">password</label>
+          <input type="password"/>
+        </div>
+        <div className="row actions">
+          <button type="submit">sign in</button>
+        </div>
+      </form>
+    );
+
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
           <nav onChange={this.switch.bind(this)}>
-            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册</label>
-            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录</label>
+            <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> sign up</label>
+            <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/>sign in</label>
           </nav>
           <div className="panes">
-            <form action="" className="signUp">
-              <div className='row'>
-                <label htmlFor="">user name</label>
-                <input type="text"/>
-              </div>
-              <div className="row">
-                <label htmlFor="">password</label>
-                <input type="password"/>
-              </div>
-              <div className="row actions">
-                <button type="submit">sign up</button>
-              </div>
-            </form>
-            <form action="" className="singIn">
-              <div className="row">
-                <label htmlFor="">user name</label>
-                <input type="text"/>
-              </div>
-              <div className="row">
-                <label htmlFor="">password</label>
-                <input type="password"/>
-              </div>
-              <div className="row actions">
-                <button type="submit">sign in</button>
-              </div>
-            </form>
+            {this.state.selected === 'signUp' ? signUpForm : null}
+            {this.state.selected === 'signIn' ? signInForm : null}
           </div>
         </div>
       </div>
